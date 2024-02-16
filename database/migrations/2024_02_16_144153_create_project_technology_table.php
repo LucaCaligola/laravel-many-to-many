61c1ca9b0a81c96189338b05f16c412ca57de4c3
LucaCaligola <luca.caligola@gmail.com>
2024-02-16 16:05:30 +0100

    working on the pivot table

---
+++ database/migrations/2024_02_16_144153_create_project_technology_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('thechnology_id');
+            $table->foreign('technology_id')->references('id')->on('technologies')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+
+            $table->primary(['thechnology_id', 'project_id']);
+             
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};
